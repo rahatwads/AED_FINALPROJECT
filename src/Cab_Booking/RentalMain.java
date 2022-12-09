d627fb0342414cdf4e0a898f0def465881984bbc
TanviKakde <114418165+TanviKakde@users.noreply.github.com>
2022-12-09 18:48:19 -0500

    Connected MYSQL Database

---
+++ src/Cab_Booking/RentalMain.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Cab_Booking;
+
+import java.sql.Connection;
+import java.lang.System.Logger;
+import java.lang.System.Logger.Level;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author tanvikakde
+ */
+public class RentalMain {
+
+    public static void main(String[] args) throws java.sql.SQLException {
+        String user = "root";
+        String password = "shivani3299";
+        String url = "jdbc:mysql://localhost:3306/BONVOYAGE";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(RentalMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        Connection c = DriverManager.getConnection(url, user, password);
+        if (c != null) {
+            System.out.print("SUCCESS");
+        } else {
+            System.out.print("FAILURE");
+        }
+
+    }
+
+}
