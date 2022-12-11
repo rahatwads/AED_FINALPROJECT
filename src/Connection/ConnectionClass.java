9cc851e38af2aabffbfc2f933ead07da27ce109d
rahatwads <114513851+rahatwads@users.noreply.github.com>
2022-12-11 02:31:54 -0500

    Connection classes with revised DB

---
+++ src/Connection/ConnectionClass.java
@@ -0,0 +1,26 @@
+
+package Connection;
+import java.sql.*; 
+
+
+public class ConnectionClass {
+     Connection c;
+    Statement s;
+    public ConnectionClass()
+    {  
+        try
+        {  
+            Class.forName("com.mysql.cj.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql://localhost:3306/CabBooking_system?zeroDateTimeBehavior=CONVERT_TO_NULL","root","shivani3299");
+            s =c.createStatement();
+        }
+        catch(Exception e)
+        { 
+            System.out.println(e);
+        }  
+    }
+     public static void main(String args[]){
+        new ConnectionClass();
+    }
+    
+}
