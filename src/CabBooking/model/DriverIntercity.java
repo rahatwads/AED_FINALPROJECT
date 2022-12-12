b9a0a1a2b701d6545039559e40846b1dad956daf
rahatwads <114513851+rahatwads@users.noreply.github.com>
2022-12-11 20:52:32 -0500

    connections

---
+++ src/CabBooking/model/DriverIntercity.java
@@ -0,0 +1,53 @@
+
+package CabBooking.model;
+
+
+public class DriverIntercity {
+        private String driver;
+    private String source;
+    private String destination;
+    private String car;
+    private String price;
+
+    public String getDriver() {
+        return driver;
+    }
+
+    public void setDriver(String driver) {
+        this.driver = driver;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getCar() {
+        return car;
+    }
+
+    public void setCar(String car) {
+        this.car = car;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+    
+    
+}
---
+++ src/CabBooking/model/Signup.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CabBooking.model;
+
+/**
+ *
+ * @author shivanirahatwad
+ */
+public class Signup {
+    private String username;
+    private String name;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhno() {
+        return phno;
+    }
+
+    public void setPhno(String phno) {
+        this.phno = phno;
+    }
+    private String phno;
+}
